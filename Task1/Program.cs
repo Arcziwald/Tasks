ae4396b18b34b9e5cea411b90139b98ab7954562
Artur Mochnia <amochnia@gmail.com>
2023-12-13 16:24:52 +0000

    added task1

---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Pracownik
+{
+    static void Main()
+    {
+        string imie;
+        string nazwisko;
+        int wiek;
+        char plec;
+        string pesel;
+        long numerPracownika;
+
+        
+        imie = "Artur";
+        nazwisko = "Mochnia";
+        wiek = 50;
+        plec = 'm';
+        pesel = "73050512345";
+        numerPracownika = 2509324094;
+
+        // Wyświetlanie danych
+        Console.WriteLine("Dane pracownika:");
+        Console.WriteLine($"Imię: {imie}");
+        Console.WriteLine($"Nazwisko: {nazwisko}");
+        Console.WriteLine($"Wiek: {wiek}");
+        Console.WriteLine($"Płeć: {plec}");
+        Console.WriteLine($"PESEL: {pesel}");
+        Console.WriteLine($"Numer pracownika: {numerPracownika}");
+    }
+}
+
