e93a0a6ec2b94a32c5e7bfce9acab503ce8977ed
Artur Mochnia <amochnia@gmail.com>
2023-12-13 16:39:44 +0000

    added task3

---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double szerokosc, dlugosc, przekatna;
+
+        Console.Write("Podaj szerokość prostokąta: ");
+        szerokosc = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Podaj długość prostokąta: ");
+        dlugosc = Convert.ToDouble(Console.ReadLine());
+
+        przekatna = Math.Sqrt(Math.Pow(szerokosc, 2) + Math.Pow(dlugosc, 2));
+
+        Console.WriteLine($"Długość przekątnej prostokąta: {przekatna}");
+    }
+}
