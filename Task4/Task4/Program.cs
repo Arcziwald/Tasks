16d1d67d5b75dbd89e74f061b164c9cdabe9d3da
Artur Mochnia <amochnia@gmail.com>
2023-12-13 16:44:17 +0000

    added task 4

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int liczbaCalkowita = 10;
+        string tekst = "Szkoła Dotneta";
+        double liczbaZmiennoprzecinkowa = 12.5;
+
+        Console.WriteLine($"Liczba całkowita: {liczbaCalkowita}");
+        Console.WriteLine($"Tekst: {tekst}");
+        Console.WriteLine($"Liczba zmiennoprzecinkowa: {liczbaZmiennoprzecinkowa}");
+    }
+}
+
