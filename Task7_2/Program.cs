95c70bb262e4d14f9896e33136863316d87c13bc
Artur Mochnia <amochnia@gmail.com>
2023-12-14 17:38:07 +0000

    added task 7/2

---
+++ Task7_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbe:");
+        int liczba = Convert.ToInt32 (Console.ReadLine());
+
+        if (liczba % 2 == 0) 
+        {
+            Console.WriteLine($"{liczba} jest liczbą parzystą");
+        }
+        else
+        {
+            Console.WriteLine($"{liczba} jest liczbą nieparzystą");
+        }
+    }
+}
