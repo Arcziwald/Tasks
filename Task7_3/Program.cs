fb495dc1f501c604dc829fa01946b6b5f158fb11
Artur Mochnia <amochnia@gmail.com>
2023-12-14 17:47:17 +0000

    added task 7/3

---
+++ Task7_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbę: ");
+        int liczba = Convert.ToInt32(Console.ReadLine());
+
+        if (liczba > 0)
+                {
+            Console.WriteLine($"{liczba} jest liczbą dodatnią");
+        }
+      else if (liczba < 0)
+        {
+            Console.WriteLine($"{liczba} jest liczbą ujemną");
+        }
+      else 
+        {
+            Console.WriteLine("Liczba to 0");
+        }
+
+    }
+}
