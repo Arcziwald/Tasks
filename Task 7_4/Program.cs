032961139b8053139d91f9746d03bc203bd148bb
Artur Mochnia <amochnia@gmail.com>
2023-12-15 20:49:40 +0000

    added Task7/4

---
+++ Task 7_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj rok: ");
+        int rok = Convert.ToInt32(Console.ReadLine());
+
+        if ((rok % 4 == 0 && rok % 100 !=0) ||  (rok % 400 == 0))
+        {
+            Console.WriteLine($"{rok} jest rokiem przestępnym");
+        }
+        else
+        {
+            Console.WriteLine($"{rok} nie jest rokiem przestępnym");
+        }
+    }
+}
+
---
+++ Task 7_5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj wiek: ");
+        int wiek = Convert.ToInt32(Console.ReadLine());
+
+        if (wiek >= 21)
+        {
+            Console.WriteLine("Możesz zostać posłem");
+        }
+
+        if (wiek >= 30)
+        {
+            Console.WriteLine("Możesz zostać senatorem");
+        }
+
+        if (wiek >= 35)
+        {
+            Console.WriteLine("Mozesz zostać premierem");
+        }
+
+        if (wiek >= 35)
+        {
+            Console.WriteLine("Mozesz zostać prezydentem");
+        }
+    }
+}
