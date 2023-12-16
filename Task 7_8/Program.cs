db128941216fcccaf935610e54eaaad4d412cc4f
Artur Mochnia <amochnia@gmail.com>
2023-12-16 15:08:52 +0000

    added Task 7/8

---
+++ Task 7_8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj wynik z matematyki");
+        int wynikMatematyka = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Podaj wynik z fizyki");
+        int wynikFizyka = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Podaj wynik z chemii");
+        int wynikChemia = Convert.ToInt32(Console.ReadLine());
+
+        bool kwalifikacja = (wynikMatematyka > 70 && wynikFizyka > 55 && wynikChemia > 45 && (wynikMatematyka + wynikFizyka + wynikChemia) > 180) || (wynikMatematyka + Math.Max(wynikFizyka, wynikChemia) > 150);
+
+        if (kwalifikacja)
+            {
+            Console.WriteLine("Kandydat dopuszczony do rekrutacji");
+        }
+        else
+        {
+            Console.WriteLine("Kandydat nie spełnia warunków rekrutacji");
+        }
+    }
+}
