d4fe49d1bf961a51537a6bff3b906ce90aa7a5ec
Artur Mochnia <amochnia@gmail.com>
2023-12-16 12:19:44 +0000

    added Task 7/6

---
+++ Task 7_6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj swój wzrost w cm: ");
+        int wzrost = Convert.ToInt32 (Console.ReadLine());
+
+        if (wzrost < 150)
+        {
+            Console.WriteLine("Jesteś krasnoludem");
+        }
+        else if (wzrost >= 150 && wzrost < 160)
+        {
+            Console.WriteLine("Jesteś hobbit");
+        }
+        else if (wzrost >= 160 && wzrost < 175)
+        {
+            Console.WriteLine("Jesteś średniego wzrostu");
+        }
+        else if (wzrost >= 175 && wzrost < 190)
+        {
+            Console.WriteLine("Jesteś wysoki");
+        }
+        else
+        {
+            Console.WriteLine("Jesteś olbrzymem");
+        }
+    }
+}
