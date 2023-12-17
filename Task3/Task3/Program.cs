ac90fe88c22aa341bb71d421110341e8ffba1b77
Artur Mochnia <amochnia@gmail.com>
2023-12-17 18:22:26 +0000

    language change in definitions

--- Task3/Task3/Program.cs
+++ Task3/Task3/Program.cs
@@ -4,15 +4,15 @@ class Program
 {
     static void Main()
     {
-        double szerokosc, dlugosc, przekatna;
+        double width, length, diagonal;
 
         Console.Write("Podaj szerokość prostokąta: ");
-        szerokosc = Convert.ToDouble(Console.ReadLine());
+        width = Convert.ToDouble(Console.ReadLine());
 
         Console.Write("Podaj długość prostokąta: ");
-        dlugosc = Convert.ToDouble(Console.ReadLine());
+        length = Convert.ToDouble(Console.ReadLine());
 
-        przekatna = Math.Sqrt(Math.Pow(szerokosc, 2) + Math.Pow(dlugosc, 2));
+        diagonal = Math.Sqrt(Math.Pow(szerokosc, 2) + Math.Pow(dlugosc, 2));
 
         Console.WriteLine($"Długość przekątnej prostokąta: {przekatna}");
     }
