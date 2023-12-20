3a5b7d0ca2207cfc38a9172b102131f0745f63a6
Artur Mochnia <amochnia@gmail.com>
2023-12-20 18:39:06 +0000

    added Task 7/11

---
+++ Task 7_11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj ocenę (1-6): ");
+        int rating = Convert.ToInt32(Console.ReadLine());
+
+        switch (rating)
+        {
+            case 6: Console.WriteLine("Celujący");
+                break;
+            case 5: Console.WriteLine("Bardzo dobry");
+                break;
+            case 4: Console.WriteLine("Dobry");
+                break;
+            case 3: Console.WriteLine("Dostateczny");
+                break;
+            case 2: Console.WriteLine("Dopuszczający");
+                break;
+            case 1: Console.WriteLine("Niedostateczny");
+                break;
+            default: Console.WriteLine("Niepoprawna ocena");
+                break;
+        }
+    }
+}
