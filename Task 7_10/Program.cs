3ff9d654b20e68b65b28f21b2f7d26e68375b6e2
Artur Mochnia <amochnia@gmail.com>
2023-12-20 13:47:32 +0000

    added Task 7/10

---
+++ Task 7_10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj długości boków trójkąta oddzielone przecinkiem");
+        string[] sides = Console.ReadLine().Split(",");
+        int a = Convert.ToInt32(sides[0]);
+        int b = Convert.ToInt32(sides[1]);
+        int c = Convert.ToInt32(sides[2]);
+
+        if (a + b > c && a + c > b && b + c > a)
+        {
+            Console.WriteLine("Mozna zbudować trójkąt");
+        }
+        else
+        {
+            Console.WriteLine("Nie można zbudować trójkąta");
+        }
+    }
+}
