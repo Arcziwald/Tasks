36ea24fe5c562d567afb0e0aa8f45bb8aaf4653e
Artur Mochnia <amochnia@gmail.com>
2023-12-21 16:28:08 +0000

    added task 7/12

---
+++ Task 7_12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+       static void Main()
+    {
+        Console.WriteLine("Podaj numer dnia tygodnia (1-7): ");
+        int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+        switch (numerDnia)
+        {
+            case 1: Console.WriteLine("Poniedziałek");
+                break;
+            case 2: Console.WriteLine("Wtorek");
+                break;
+            case 3: Console.WriteLine("Środa");
+                break;
+            case 4: Console.WriteLine("Czwartek");
+                break;
+            case 5: Console.WriteLine("Piątek");
+                break; 
+            case 6: Console.WriteLine("Sobota");
+                break;  
+            case 7: Console.WriteLine("Niedziela");
+                break;  
+            default: Console.WriteLine("Niepoprawny numer dnia tygodnia");
+                break;
+        }
+    }
+}
