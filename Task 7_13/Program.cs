b8a4c23de2de4516f7bb7e80fbbbffac79245fa8
Artur Mochnia <amochnia@gmail.com>
2023-12-21 16:44:57 +0000

    added task 7/13

---
+++ Task 7_13/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj pierwszą liczbę: ");
+        double number1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Podaj drugą liczbę: ");
+        double number2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Podaj numer operacji do wykonania: ");
+        Console.WriteLine("1. Dodawanie");
+        Console.WriteLine("2. Odejmowanie");
+        Console.WriteLine("3. Mnożenie");
+        Console.WriteLine("4. Dzielenie");
+
+        int operacja = Convert.ToInt32(Console.ReadLine());
+        double result;
+
+        switch(operacja)
+        {
+            case 1:
+                result = number1 + number2;
+                break;
+            case 2:
+                result = number1 - number2;
+                break;
+            case 3:
+                result = number1 * number2;
+                break;
+            case 4:
+                if (number2 !=0)
+                {
+                    result = number1 / number2;
+                }
+                else
+                {
+                    Console.WriteLine("Nie można dzielić przez 0");
+                    return;
+                }
+                break;
+                default: Console.WriteLine("Niepoprawny numer operacji");
+                return;
+        }
+        Console.WriteLine($"Twój wynik to:{result}");
+    }
+}
