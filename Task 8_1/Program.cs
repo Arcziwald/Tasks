1f8cd1abaa4462fda8c24ec0387ef9848d63e243
Artur Mochnia <amochnia@gmail.com>
2023-12-22 21:23:48 +0000

    added Task 8/1

---
+++ Task 8_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int primeNumbersCount = 0;
+
+        for (int i = 2; i <= 100; i++)
+        {
+            if (IsPrime(i))
+            {
+                primeNumbersCount++;
+            }
+        }
+        Console.WriteLine($"ilość liczb pierwszych w zakresie wynosi: {primeNumbersCount}");
+    }
+    static bool IsPrime(int number)
+    {
+        if (number <= 1) return false;
+        if (number == 2) return true;
+
+        for (int i = 2; i * i <= number; i++)
+        {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
