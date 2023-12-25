f36f775860829c06b5ab50e85f202bce4893cec8
Artur Mochnia <amochnia@gmail.com>
2023-12-25 17:15:07 +0000

    added Task 8/4

---
+++ Task 8_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbę całkowitą: ");
+        int maxNumber;
+        
+        if(!int.TryParse(Console.ReadLine(), out maxNumber))
+        {
+            Console.WriteLine("To nie jest prawidłowa liczba całkowita");
+            return;
+        }
+
+        int currentNumber = 1;
+
+        for(int i = 1; currentNumber < maxNumber; i++)
+        {
+            for (int j = 0; j < i && currentNumber <= maxNumber; j++)
+            {
+                Console.WriteLine(currentNumber + " ");
+                currentNumber++;
+            }
+            Console.WriteLine();
+        }
+        
+    }
+}
