4767de4c713ecbd616a6e5a4ca3e32c4f37a8c48
Artur Mochnia <amochnia@gmail.com>
2023-12-27 14:30:16 +0000

    added Task 8/6

---
+++ Task 8_6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double sum = 0;
+
+        for(int i = 1; i <= 20; i++)
+        {
+            sum += 1.0/ i;
+        }
+        Console.WriteLine($"Suma wynosi: {sum}");
+    }
+}
