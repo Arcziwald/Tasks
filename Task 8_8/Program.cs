6f7ebe5f55d69dbb22726d518fb6018a2618f265
Artur Mochnia <amochnia@gmail.com>
2023-12-29 16:19:18 +0000

    added task 8/8

---
+++ Task 8_8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Wprowadż ciąg znaków: ");
+
+        string enteredText = Console.ReadLine();
+        string reverseText = ReverseText(enteredText);
+
+        Console.WriteLine($"Odwrócony ciąg znaków: {reverseText}");
+    }
+        static string ReverseText(string text)
+    {
+        char[] chars = text.ToCharArray();
+        Array.Reverse( chars );
+        return new string( chars );
+    }
+}
