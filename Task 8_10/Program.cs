6b7d479ba46156b777d613d6da411329bcfbee9b
Artur Mochnia <amochnia@gmail.com>
2023-12-29 16:19:39 +0000

    added Task 8/9

---
+++ Task 8_10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj pierwszą liczbę: ");
+        int number1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Podaj drugą liczbę: ");
+        int number2 = int.Parse(Console.ReadLine());
+
+        int nww = FindNWW(number1, number2);
+
+        Console.WriteLine($"Najmniejsza wspólna wielokrotność dla {number1} i {number2} to {nww}");
+    }
+
+    static int FindNWW(int a, int b)
+    {
+        return Math.Abs(a * b) / FindNWD(a, b);
+    }
+
+    static int FindNWD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int t = b;
+            b = a % b;
+            a = t;
+        }
+        return a;
+    }
+}
---
+++ Task 8_9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbę dziesiętną: ");
+        int decimalNumber;
+        if(!int.TryParse(Console.ReadLine(), out decimalNumber))
+        {
+            Console.WriteLine("TO nie jest prawidłowa liczba całkowita");
+            return;
+        }
+        string binaryNumber = Convert.ToString(decimalNumber, 2);
+
+        Console.WriteLine($"Liczba binarna: {binaryNumber}");
+    }
+}
