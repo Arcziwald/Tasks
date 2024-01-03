41c7ba2e96606e3a9efcd04dc24d7b7255c2abba
Artur Mochnia <amochnia@gmail.com>
2024-01-03 13:38:42 +0000

    added user class

--- Library/User.cs
+++ Library/User.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    internal class User
+    public class User(int id, string name, string email)
     {
+        public int Id { get; set; } = id;
+        public string Name { get; set; } = name;
+        public string Email { get; set; } = email;
+        public List<Book> BorrowedBooks { get; set; } = [];
     }
 }
