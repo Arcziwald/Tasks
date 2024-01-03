7187bb4dc7a29ba978287e12e32381262937bd7a
Artur Mochnia <amochnia@gmail.com>
2024-01-03 13:37:15 +0000

    adde book class

--- Library/Book.cs
+++ Library/Book.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    internal class Book
+    public class Book(string title, string author, int yearOfPublication)
     {
+        public string Title { get; set; } = title;
+        public string Author { get; set; } = author;
+        public int YearOfPublication { get; set; } = yearOfPublication;
+        public bool Available { get; set; } = true;
+                      
+        public override string ToString()
+        {
+            return $"{Title} - {Author}, {YearOfPublication}";
+        }
     }
 }
