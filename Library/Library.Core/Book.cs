2095246cc1f85cda55f5d9913a98bf132fe2baa6
Artur Mochnia <amochnia@gmail.com>
2024-01-24 15:51:02 +0000

    program refactoring

---
+++ Library/Library.Core/Book.cs
@@ -0,0 +1,24 @@
+﻿namespace Library.Core
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int YearOfPublication { get; set; }
+        public int? BorrowedById { get; set; }
+        public bool Available { get; set; } = true;
+
+        public Book(string title, string author, int yearOfPublication)
+        {
+            Title = title;
+            Author = author;
+            YearOfPublication = yearOfPublication;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Author}, {YearOfPublication}";
+        }
+    }
+}
---
+++ Library/Library.Core/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Library.Core
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public List<Book> BorrowedBooks { get; set; } = new List<Book>();
+
+        public User(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+    }
+}
